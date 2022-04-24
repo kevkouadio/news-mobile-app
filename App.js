b5b4fb08fb75c1b1b18bd97a1ebbe2f7eb71cab6
kevkouadio <kevkouadio@gmail.com>
2022-04-24 09:50:53 -0400

    add of search for article

--- App.js
+++ App.js
@@ -1,22 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import  Search  from "./app/Search";
+import { StyleSheet, Text, View, Dimensions, ScrollView } from 'react-native';
 import ArticlesCard from './app/ArticlesCard';
-import { ActivityIndicator, FlatList } from 'react-native';
-import { Card } from 'react-native-paper'
+
 
 export default function App() {
   
   return (
+    <ScrollView>
     <View style={styles.container}>
-      <Search />
       <ArticlesCard />
       <StatusBar style="auto" />
-      {/* <View style={styles.footer}>
-      <Text style={styles.textStyle}>Bottom View</Text>
-      </View> */}
     </View>
+    </ScrollView>
   );
 }
 
@@ -25,16 +21,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#EE5407',
     alignItems: 'center',
-    //justifyContent: 'space-between',
-  },
-  footer: {
-    width: '100%',
-    height: 50,
-    backgroundColor: '#EE5407',
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'absolute', //Here is the trick
-    bottom: 0, //Here is the trick
   },
   textStyle: {
     color: '#fff',
--- app/ArticlesCard.js
+++ app/ArticlesCard.js
@@ -1,33 +1,56 @@
 import React, { useEffect, useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ActivityIndicator, FlatList, Linking } from 'react-native';
-import { Card } from 'react-native-paper';
-//import Dimensions from 'Dimensions';
+import { StyleSheet, Text, View, ActivityIndicator, FlatList, Linking, Dimensions } from 'react-native';
+import { Card, Searchbar } from 'react-native-paper';
+
+const { width, height } = Dimensions.get('window');
 
 const ArticlesCard = () => {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
 
-  const getMovies = async () => {
-    try {
-      const response = await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=c221b6edf9704a38bb066216200581f3');
-      const json = await response.json();
-      setData(json.articles);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setLoading(false);
+  const onChangeSearch = query => setSearchQuery(query);
+
+  const getArticles = async () => {
+    if (searchQuery != '') {
+      try {
+        const response = await fetch('https://newsapi.org/v2/everything?q=' + searchQuery +
+          '&apiKey=' + 'eecca520005045a2b9c19a88e5c74bfc');
+        const json = await response.json();
+        setData(json.articles);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      try {
+        const response = await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=' + 'eecca520005045a2b9c19a88e5c74bfc');
+        const json = await response.json();
+        setData(json.articles);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
     }
   }
 
   useEffect(() => {
-    getMovies();
-  }, []);
+    getArticles();
+  }, [data]);
   //console.log(data)
+  //console.log(searchQuery)
   return (
-    <View >
+    <View>
+      <Searchbar
+        style={styles.input}
+        placeholder="Search for article"
+        onChangeText={onChangeSearch}
+        value={searchQuery}
+      />
       {isLoading ? <ActivityIndicator /> : (
-        <FlatList 
+        <FlatList
           data={data}
           keyExtractor={({ id }, index) => id}
           renderItem={({ item }) => (
@@ -44,28 +67,46 @@ const ArticlesCard = () => {
 
 const styles = StyleSheet.create({
   cardText: {
-    textAlign: 'left', 
-    fontSize: 20, 
+    textAlign: 'left',
+    fontSize: width * 0.04,
     //alignItems:"center", 
-    position:"relative", 
-    marginRight:100
+    position: "relative",
+    marginRight: 110,
+    fontFamily: 'TimesNewRomanPS-BoldMT'
+    // left: width * .0,
+    // right: width * .1,
   },
   card: {
-    width: 400,
+    //width: width,
     height: 100,
     flex: 1,
     flexDirection: 'row',
     flexWrap: "wrap",
     textAlign: 'left',
     alignItems: "center",
-    marginBottom: 5,
+    marginBottom: 2,
+    left: width * .09,
+    //right: width * .0,
+    width: width * .95
+    //borderEndColor: "black"
   },
   image: {
-    height: 100,
-    width: 100,
+    // height: 10,
+    width: width * .25,
     position: "absolute",
-    marginLeft: 300,
-    borderRadius: 4
-  }
+    marginLeft: width * .7,
+    borderRadius: 4,
+    // left: width * .1,
+    // right: width * .0,
+    height: height * 0.18,
+  },
+  input: {
+    left: width * .0,
+    right: width * .1,
+    width: width * 0.95,
+    margin: width * 0.09,
+    backgroundColor: '#fff',
+    borderRadius: 25,
+  },
 });
 export default ArticlesCard;
